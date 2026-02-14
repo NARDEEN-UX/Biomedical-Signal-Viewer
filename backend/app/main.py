5fdeadcb9889ca547b4a12282bb6bcc2f91a2642
Fahd Ahmed <afahd9002@gmail.com>
2026-02-14 17:49:55 +0200

    Acoustic signals

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.endpoints import router
+
 
 app = FastAPI(title="Biomedical Signal Viewer API")
 
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router)
+
 
 @app.get("/")
 def health_check():

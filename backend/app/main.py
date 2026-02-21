4f9dcf6312658f5879f01438845843a4cdb05239
Fahd Ahmed <afahd9002@gmail.com>
2026-02-22 01:18:49 +0200

    test the endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.Acoustic_Signals.api.endpoints import acoustic_router
 from app.MicroBiome.api.endpoint import microbiome_rouuter
+from app.Market.api.endpoints import market_router
 app = FastAPI(title="Biomedical Signal Viewer API")
 
 # --- CHANGE 1: Add specific IP addresses ---
@@ -23,6 +24,7 @@ app.add_middleware(
 
 app.include_router(acoustic_router)
 app.include_router(microbiome_rouuter)
+app.include_router(market_router)
 @app.get("/")
 def health_check():
     return {"status": "Biomedical API is running"}
